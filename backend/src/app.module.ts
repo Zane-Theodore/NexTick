3472a4f27faf8aa9b320dca3668188f4fca0294f
thienphuc <tthienphuc1612@gmail.com>
2026-04-25 17:11:01 +0700

    feat(database): add database module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { CandlesModule } from '@modules/candles/candles.module';
+import { DatabaseModule } from '@modules/database/database.module';
 
 @Module({
-  imports: [CandlesModule],
+  imports: [CandlesModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
