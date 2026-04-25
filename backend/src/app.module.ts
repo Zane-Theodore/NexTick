654c0bc10a1b1803a787afb02682df7198ce0f90
thienphuc <tthienphuc1612@gmail.com>
2026-04-25 17:12:21 +0700

    feat(kafka): add kafka module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 
 import { CandlesModule } from '@modules/candles/candles.module';
 import { DatabaseModule } from '@modules/database/database.module';
+import { KafkaModule } from '@modules/kafka/kafka.module';
 
 @Module({
-  imports: [CandlesModule, DatabaseModule],
+  imports: [CandlesModule, DatabaseModule, KafkaModule],
   controllers: [AppController],
   providers: [AppService],
 })
